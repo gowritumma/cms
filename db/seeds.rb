f2ff2a959405a879105614b799bfbea61b80d418
gowri <gowri.tumma@gmail.com>
2020-01-15 12:14:48 -0500

    seed data for first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+AdminUser.create(
+	:firstname => 'test',
+	:lastname => 'test',
+	:email => 'test@test.com',
+	:username => 'testuser',
+	:password => 'test'
+	)
